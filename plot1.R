d38a49196db2f2e520bfdf7ac8a5992fea0638fd
Yeo Chai Heng <clivant@gmail.com>
2014-10-12 17:04:11 +0800

    Removed duplicated column labels values in data frames

--- plot1.R
+++ plot1.R
@@ -8,7 +8,7 @@ data <- read.table(file='household_power_consumption.txt'
                                   'Date', 'Time', 'Global_active_power'
                                    , 'Global_reactive_power', 'Voltage'
                                    , 'Global_intensity', 'Sub_metering_1'
-                                   , 'Sub_metering_1', 'Sub_metering_3'
+                                   , 'Sub_metering_2', 'Sub_metering_3'
                                    )
                    , colClasses= c(
                                   'character', 'character', 'numeric'
