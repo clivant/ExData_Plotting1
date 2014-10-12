aa98e1707b9abe6837fd8e55741d8ec4907af412
Yeo Chai Heng <clivant@gmail.com>
2014-10-12 16:34:24 +0800

    Initial commit of plot1.R and plot1.png

---
+++ plot1.R
@@ -0,0 +1,24 @@
+# Read only the required data to speed things up, assuming that data file is located in the working directory
+data <- read.table(file='household_power_consumption.txt'
+                   , header = TRUE
+                   , sep = ';'
+                   , skip = 66636
+                   , nrows = 2880
+                   , col.names = c(
+                                  'Date', 'Time', 'Global_active_power'
+                                   , 'Global_reactive_power', 'Voltage'
+                                   , 'Global_intensity', 'Sub_metering_1'
+                                   , 'Sub_metering_1', 'Sub_metering_3'
+                                   )
+                   , colClasses= c(
+                                  'character', 'character', 'numeric'
+                                   , 'numeric', 'numeric', 'numeric'
+                                   , 'numeric', 'numeric', 'numeric'
+                                  )
+                   )
+
+# Plot graph 
+png('plot1.png', height = 450, width = 450)
+hist(data$Global_active_power, xlab = 'Global Active Power (kilowatts)', col='red')
+dev.off()
+
